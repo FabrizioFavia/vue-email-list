7542d77d35c226fd635afac5f724622b397a94fe
FabrizioFavia <personal.favia@gmail.com>
2023-05-17 15:50:17 +0200

    feat(html/css/js): finished project

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
-    },
-    methods: {
+createApp({
+  data() {
+    return {
+      requestPath: "https://flynn.boolean.careers/exercises/api/random/mail",
+      emailArray: [],
+      answer: null,
     }
-  }).mount('#app')
+  },
+  methods: {
+
+  },
+  mounted() {
+
+    for (let i = 1; i < 11; i++) {
+      axios.get(this.requestPath).then((risposta) => {
+        this.answer = risposta.data.response;
+        this.emailArray.push(this.answer);
+      })
+    }
+    console.log(this.emailArray);
+  }
+
+}).mount('#app')
